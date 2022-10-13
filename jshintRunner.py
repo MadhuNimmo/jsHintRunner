23ddffd48b3081a5bd713b5df7b6f0817ab64ea8
Madhurima Chakraborty <56744323+MadhuNimmo@users.noreply.github.com>
2022-10-12 18:07:51 -0700

    Update jshintRunner.py

--- jshintRunner.py
+++ jshintRunner.py
@@ -3,29 +3,29 @@ import re
 import sys
 import subprocess
 
-arr={};
-
-def runner(filepath):
+def runner(filepath,arr):
         if os.path.isdir(filepath):
-                for filename in os.listdir(filepath):
-                        f = os.path.join(filepath, filename)
-                        runjshint(f,arr)
+                for root, subFolders, files in os.walk(filepath):
+                        for file in files:
+                                if(file.endswith(".js")):
+                                        f = os.path.join(root, file)
+                                        runjshint(f,arr)
         elif os.path.isfile(filepath):
                 runjshint(filepath,arr)
         print(arr)
 def runjshint(f,arr):
-        if os.path.isfile(f):
-                bashCommand = "jshint "+f
-                process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-                output, error = process.communicate()
-                outarr = output.decode("utf-8").split('\n')
-                for item in outarr:
-                        if "ES6" in item:
-                                keyname = item.split(", ")[-1]
-                                if keyname in arr.keys():
-                                        arr[keyname]+=1
-                                else:
-                                        arr[keyname]=1
+        bashCommand = "jshint "+f
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        outarr = output.decode("utf-8").split('\n')
+        for item in outarr:
+                if "ES6" in item:
+                        keyname = item.split(", ")[-1]
+                        if keyname in arr.keys():
+                                arr[keyname]+=1
+                        else:
+                                arr[keyname]=1
         return arr
 if __name__ == "__main__":
-            runner(sys.argv[1])
+            arr={};
+            runner(sys.argv[1],arr)
